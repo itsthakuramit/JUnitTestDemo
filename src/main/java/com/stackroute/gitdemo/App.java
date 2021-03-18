c7e794763d61a0a27713be6862fc221d3e07fdf3
bharat_kumar <bharat_kumar@hcl.com>
2021-03-18 16:44:06 +0530

    Update App.java

--- src/main/java/com/stackroute/gitdemo/App.java
+++ src/main/java/com/stackroute/gitdemo/App.java
@@ -1,11 +1,9 @@
 package com.stackroute.gitdemo;
 
-// New Edit
+
 public class App {
 	public static void main(String[] args) {
-		// Edit by Durai
-		System.out.println("Welcome to Java");
 		
-		// new one - bh
+		System.out.println("Welcome to Java");
 	}
 }
